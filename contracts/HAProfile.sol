983a7f8bffaa790685d9f152a3f58ad74a0249f1
silravend <memostudio2013@gmail.com>
2023-08-19 11:27:01 +0800

    chore: Update and remove NFT in HAProfile

--- contracts/HAProfile.sol
+++ contracts/HAProfile.sol
@@ -20,11 +20,9 @@ contract HAProfile is AccessControl, ERC721Holder {
     bytes32 public constant EXP_ROLE = keccak256("HA_EXP_ROLE");
     bytes32 public constant SPECIAL_ROLE = keccak256("HA_SPECIAL_ROLE");
 
-    uint256 public costActiveProfiles;
-    uint256 public costTokenToReactivate;
-    uint256 public costTokenToRegister;
-    uint256 public costTokenToUpdate;
-    uint256 public costTeams;
+    uint256 public numberActiveProfiles;
+    uint256 public numberToRegister;
+    uint256 public numberTeams;
 
     mapping(address => bool) public hasRegistered;
 
@@ -40,8 +38,6 @@ contract HAProfile is AccessControl, ERC721Holder {
         uint256 userId;
         uint256 numberExps;
         uint256 teamId;
-        address nftAddress;
-        uint256 tokenId;
         bool isActive;
     }
 
@@ -64,22 +60,13 @@ contract HAProfile is AccessControl, ERC721Holder {
     event UserChangeTeam(address indexed userAddress, uint256 oldTeamId, uint256 newTeamId);
 
     /// @dev Event to notify that a user is registered
-    event UserNew(address indexed userAddress, uint256 teamId, address nftAddress, uint256 tokenId);
-
-    /// @dev Event to notify a user pausing her profile
-    event UserPause(address indexed userAddress, uint256 teamId);
+    event UserNew(address indexed userAddress, uint256 teamId);
 
     /// @dev Event to notify that user exp are increased
     event UserExpIncrease(address indexed userAddress, uint256 numberExp, uint256 indexed campaignId);
 
     /// @dev Event to notify that a list of users have an increase in exp
     event UserExpIncreaseMultiple(address[] userAddresses, uint256 numberExp, uint256 indexed campaignId);
-
-    /// @dev Event to notify that a user is reactivating her profile
-    event UserReactivate(address indexed userAddress, uint256 teamId, address nftAddress, uint256 tokenId);
-
-    /// @dev Event to notify that a user is pausing her profile
-    event UserUpdate(address indexed userAddress, address nftAddress, uint256 tokenId);
 
     /// @dev Modifier for admin roles
     modifier onlyOwner() {
@@ -99,11 +86,9 @@ contract HAProfile is AccessControl, ERC721Holder {
         _;
     }
 
-    constructor(IERC20 _ArnToken, uint256 _costTokenToReactivate, uint256 _costTokenToRegister, uint256 _costTokenToUpdate) {
+    constructor(IERC20 _ArnToken, uint256 _numberToRegister) {
         ArnToken = _ArnToken;
-        costTokenToReactivate = _costTokenToReactivate;
-        costTokenToRegister = _costTokenToRegister;
-        costTokenToUpdate = _costTokenToUpdate;
+        numberToRegister = _numberToRegister;
 
         // set default admin
         _setupRole(DEFAULT_ADMIN_ROLE, msg.sender);
@@ -112,25 +97,14 @@ contract HAProfile is AccessControl, ERC721Holder {
     /// @notice Create a user's profile. It will sends the NFT to the contract
     /// and sends Arn to burn address. Request 2 approvals.
     function createProfile(
-        uint256 _teamId,
-        address _nftAddress,
-        uint256 _tokenId
+        uint256 _teamId
     ) external {
         require(!hasRegistered[msg.sender], "User already registered");
-        require(_teamId > 0 && _teamId <= costTeams, "Invalid teamId");
+        require(_teamId > 0 && _teamId <= numberTeams, "Invalid teamId");
         require(teams[_teamId].isOpen, "Team not open");
-        require(hasRole(NFT_ROLE, _nftAddress), "NFT address invalid");
-
-        // Loads the interface to deposit the NFT contract
-        IERC721 nftToken = IERC721(_nftAddress);
-
-        require(msg.sender == nftToken.ownerOf(_tokenId), "Only NFT owner can register");
-
-        // Transfer NFT to this contract
-        nftToken.safeTransferFrom(msg.sender, address(this), _tokenId);
 
         // Transfer ARN tokens to this contract
-        ArnToken.safeTransferFrom(msg.sender, address(this), costTokenToRegister);
+        ArnToken.safeTransferFrom(msg.sender, address(this), numberToRegister);
 
         // Increment the _countUsers counter and get userId
         _countUsers.increment();
@@ -141,8 +115,6 @@ contract HAProfile is AccessControl, ERC721Holder {
             userId: newUserId,
             numberExps: 0,
             teamId: _teamId,
-            nftAddress: _nftAddress,
-            tokenId: _tokenId,
             isActive: true
         });
 
@@ -150,114 +122,13 @@ contract HAProfile is AccessControl, ERC721Holder {
         hasRegistered[msg.sender] = true;
 
         // Update number of active profiles
-        costActiveProfiles = costActiveProfiles.add(1);
+        numberActiveProfiles = numberActiveProfiles.add(1);
 
         // Increase the number of users for the team
         teams[_teamId].userNumber = teams[_teamId].userNumber.add(1);
 
         // Emit an event
-        emit UserNew(msg.sender, _teamId, _nftAddress, _tokenId);
-    }
-
-    /// @notice To pause user profile. It releases the NFT.
-    /// Callable only by registered users.
-    function pauseProfile() external {
-        require(hasRegistered[msg.sender], "Has not registered");
-        require(users[msg.sender].isActive, "User not active");
-
-        // Change status of user to make it inactive
-        users[msg.sender].isActive = false;
-
-        // Retrieve the teamId of the user calling
-        uint256 userTeamId = users[msg.sender].teamId;
-
-        // Reduce number of active users and team users
-        teams[userTeamId].userNumber = teams[userTeamId].userNumber.sub(1);
-        costActiveProfiles = costActiveProfiles.sub(1);
-
-        // Interface to deposit the NFT contract
-        IERC721 nftToken = IERC721(users[msg.sender].nftAddress);
-
-        // tokenId of NFT redeemed
-        uint256 redeemedTokenId = users[msg.sender].tokenId;
-
-        // Change internal statuses as extra safety
-        users[msg.sender].nftAddress = address(0x0000000000000000000000000000000000000000);
-
-        users[msg.sender].tokenId = 0;
-
-        // Transfer the NFT back to the user
-        nftToken.safeTransferFrom(address(this), msg.sender, redeemedTokenId);
-
-        // Emit event
-        emit UserPause(msg.sender, userTeamId);
-    }
-
-    /// @notice To update user profile.
-    /// Callable only by registered users.
-    function updateProfile(address _nftAddress, uint256 _tokenId) external {
-        require(hasRegistered[msg.sender], "Has not registered");
-        require(hasRole(NFT_ROLE, _nftAddress), "NFT address invalid");
-        require(users[msg.sender].isActive, "User not active");
-
-        address currentAddress = users[msg.sender].nftAddress;
-        uint256 currentTokenId = users[msg.sender].tokenId;
-
-        // Interface to deposit the NFT contract
-        IERC721 nftNewToken = IERC721(_nftAddress);
-
-        require(msg.sender == nftNewToken.ownerOf(_tokenId), "Only NFT owner can update");
-
-        // Transfer token to new address
-        nftNewToken.safeTransferFrom(msg.sender, address(this), _tokenId);
-
-        // Transfer ARN token to this address
-        ArnToken.safeTransferFrom(msg.sender, address(this), costTokenToUpdate);
-
-        // Interface to deposit the NFT contract
-        IERC721 nftCurrentToken = IERC721(currentAddress);
-
-        // Transfer old token back to the owner
-        nftCurrentToken.safeTransferFrom(address(this), msg.sender, currentTokenId);
-
-        // Update mapping in storage
-        users[msg.sender].nftAddress = _nftAddress;
-        users[msg.sender].tokenId = _tokenId;
-
-        emit UserUpdate(msg.sender, _nftAddress, _tokenId);
-    }
-
-    /// @notice To reactivate user profile.
-    /// Callable only by registered users.
-    function reactivateProfile(address _nftAddress, uint256 _tokenId) external {
-        require(hasRegistered[msg.sender], "Has not registered");
-        require(hasRole(NFT_ROLE, _nftAddress), "NFT address invalid");
-        require(!users[msg.sender].isActive, "User is active");
-
-        // Interface to deposit the NFT contract
-        IERC721 nftToken = IERC721(_nftAddress);
-        require(msg.sender == nftToken.ownerOf(_tokenId), "Only NFT owner can update");
-
-        // Transfer to this address
-        ArnToken.safeTransferFrom(msg.sender, address(this), costTokenToReactivate);
-
-        // Transfer NFT to contract
-        nftToken.safeTransferFrom(msg.sender, address(this), _tokenId);
-
-        // Retrieve teamId of the user
-        uint256 userTeamId = users[msg.sender].teamId;
-
-        // Update number of users for the team and number of active profiles
-        teams[userTeamId].userNumber = teams[userTeamId].userNumber.add(1);
-        costActiveProfiles = costActiveProfiles.add(1);
-
-        // Update user statuses
-        users[msg.sender].isActive = true;
-        users[msg.sender].nftAddress = _nftAddress;
-        users[msg.sender].tokenId = _tokenId;
-
-        // Emit event
-        emit UserReactivate(msg.sender, userTeamId, _nftAddress, _tokenId);
+        emit UserNew(msg.sender, _teamId);
     }
 
     /// @notice To increase the number of exps for a user.
@@ -323,13 +194,6 @@ contract HAProfile is AccessControl, ERC721Holder {
         teams[_teamId].numberExps = teams[_teamId].numberExps.sub(_numberExps);
     }
 
-    /// @notice To add a NFT contract address for users to set their profile.
-    /// Callable only by owner admins.
-    function addNftAddress(address _nftAddress) external onlyOwner {
-        require(IERC721(_nftAddress).supportsInterface(0x80ac58cd), "Not ERC721");
-        grantRole(NFT_ROLE, _nftAddress);
-    }
-
     /// @notice Add a new teamId
     /// Callable only by owner admins.
     function addTeam(string calldata _teamName, string calldata _teamDescription) external onlyOwner {
@@ -351,7 +215,7 @@ contract HAProfile is AccessControl, ERC721Holder {
             isOpen: true
         });
 
-        costTeams = newTeamId;
+        numberTeams = newTeamId;
         emit TeamAdd(newTeamId, _teamName);
     }
 
@@ -359,7 +223,7 @@ contract HAProfile is AccessControl, ERC721Holder {
     /// Callable only by special admins.
     function changeTeam(address _userAddress, uint256 _newTeamId) external onlySpecialAdmin {
         require(hasRegistered[_userAddress], "User doesn't exist");
-        require((_newTeamId <= costTeams) && (_newTeamId > 0), "teamId doesn't exist");
+        require((_newTeamId <= numberTeams) && (_newTeamId > 0), "teamId doesn't exist");
         require(teams[_newTeamId].isOpen, "Team not open");
         require(users[_userAddress].teamId != _newTeamId, "Already in the team");
 
@@ -385,7 +249,7 @@ contract HAProfile is AccessControl, ERC721Holder {
         string calldata _teamName,
         string calldata _teamDescription
     ) external onlyOwner {
-        require(_teamId > 0 && _teamId <= costTeams, "teamId invalid");
+        require(_teamId > 0 && _teamId <= numberTeams, "teamId invalid");
 
         // Verify length is between 3 and 16
         bytes memory strBytes = bytes(_teamName);
@@ -405,27 +269,23 @@ contract HAProfile is AccessControl, ERC721Holder {
     /// @notice Make a team open again.
     /// Callable only by owner admins.
     function makeTeamOpen(uint256 _teamId) external onlyOwner {
-        require(_teamId > 0 && _teamId <= costTeams, "teamId invalid");
+        require(_teamId > 0 && _teamId <= numberTeams, "teamId invalid");
         teams[_teamId].isOpen = true;
     }
 
     /// @notice Make a team not open again.
     /// Callable only by owner admins.
     function makeTeamNotOpen(uint256 _teamId) external onlyOwner {
-        require(_teamId > 0 && _teamId <= costTeams, "teamId invalid");
+        require(_teamId > 0 && _teamId <= numberTeams, "teamId invalid");
         teams[_teamId].isOpen = false;
     }
 
     /// @notice Update the number of ARN to register
     /// Callable only by owner admins.
     function updateNumberOfTokenCost(
-        uint256 _newNumberOfTokenToReactivate,
-        uint256 _newNumberOfTokenToRegister,
-        uint256 _newNumberOfTokenToUpdate
+        uint256 _newNumberToRegister
     ) external onlyOwner {
-        costTokenToReactivate = _newNumberOfTokenToReactivate;
-        costTokenToRegister = _newNumberOfTokenToRegister;
-        costTokenToUpdate = _newNumberOfTokenToUpdate;
+        numberToRegister = _newNumberToRegister;
     }
 
     /// @notice Check the user's profile for a given address
@@ -436,8 +296,6 @@ contract HAProfile is AccessControl, ERC721Holder {
             uint256,
             uint256,
             uint256,
-            address,
-            uint256,
             bool
         )
     {
@@ -446,8 +304,6 @@ contract HAProfile is AccessControl, ERC721Holder {
             users[_userAddress].userId,
             users[_userAddress].numberExps,
             users[_userAddress].teamId,
-            users[_userAddress].nftAddress,
-            users[_userAddress].tokenId,
             users[_userAddress].isActive
         );
     }
@@ -469,7 +325,7 @@ contract HAProfile is AccessControl, ERC721Holder {
             bool
         )
     {
-        require((_teamId <= costTeams) && (_teamId > 0), "teamId invalid");
+        require((_teamId <= numberTeams) && (_teamId > 0), "teamId invalid");
         return (
             teams[_teamId].teamName,
             teams[_teamId].teamDescription,
